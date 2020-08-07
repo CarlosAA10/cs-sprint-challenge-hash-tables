1a89bf34660f1063926af690ed5e871e8b89bee4
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-08-07 12:50:22 -0500

    finished no_dups, working on ex5, still missing one more to meet MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,39 @@
+# cache to store values that match
+cache = {}
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
+    # while loop to allow me to loop through elements
+    # i'll have two pointers 
+    if length == 1:
+        return None
+    first_pointer = 0
+    second_pointer = 1
 
-    return None
+    while first_pointer != length - 1:
+        # print(second_pointer,'second index after each itteration')
+
+        if second_pointer == length:
+            first_pointer += 1
+            second_pointer = first_pointer + 1
+
+        elif weights[first_pointer] + weights[second_pointer] == limit: # what returns my tuple and passes test
+            cache[weights[first_pointer]] = (second_pointer, first_pointer)
+            pair = cache[weights[first_pointer]]
+
+        second_pointer += 1
+
+    if pair:
+        return pair
+    else:
+        return None
+
+
+
+weights_3 = [4, 6, 10, 15, 16]
+# arr = [4,4]
+# print(get_indices_of_item_weights(arr, 2, 8))
+print(get_indices_of_item_weights(weights_3, 5, 21))
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,29 @@
 # Your code here
 
-
+cache = {}
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+
+    result = [] 
+
+    for file in files:
+
+        chopped = file.split("/")
+
+        # return chopped[-1] returns last element in chopped list, our query if it exists
+
+        cache[chopped[-1]] = file # creates dictoinary with last thing as key, and value as the whole pathj
+
+
+    for query in queries:
+
+        if query in cache:
+            result.append(cache[query])
+
 
     return result
 
