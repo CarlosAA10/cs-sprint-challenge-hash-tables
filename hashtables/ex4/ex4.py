a5e5077287ec1aa6e016b73a3248bc54d2b29bc1
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-08-07 15:10:24 -0500

    Carlos-Arguello

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,32 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    result = [] 
+    for num in a: # correctly creates negative and positive value in cache for each number 
+
+        if num not in cache and num > 0:
+            cache[num] = "positive"
+
+        if num not in cache and num < 0:
+
+            cache[num] = "negative"
+
+    for num in a:
+
+        if num < 0:
+
+            if num in cache and abs(num) in cache:
+
+                result.append(abs(num)) # adds our negative number turned positive to results
+
+    
+
+
 
     return result
+    # return cache
 
 
 if __name__ == "__main__":
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,13 @@
 # Your code here
 
-cache = {}
+
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
     result = [] 
 
@@ -15,17 +16,32 @@ def finder(files, queries):
         chopped = file.split("/")
 
         # return chopped[-1] returns last element in chopped list, our query if it exists
+        # since there are repeated end values but different whole paths, i can still check the ending now
+        # and if i do in, i am referring to my created object/dict which will keep the 0(1) complexity 
+        if chopped[-1] in cache:
+            cache[chopped[-1]].append(file) # we add that path to this key that will contain 
+            # a list of all paths that include its ending path
 
-        cache[chopped[-1]] = file # creates dictoinary with last thing as key, and value as the whole pathj
+        else:
+            cache[chopped[-1]] = [file] # creates dictoinary with last thing as key, and value as the whole pathj
 
 
     for query in queries:
 
         if query in cache:
-            result.append(cache[query])
+            result += cache[query]
+            # result.append(cache[query]) 
 
 
     return result
+
+# arr = ["chicken", "chickkk"]
+
+# result = ["cow"]
+
+# result += arr
+
+# print(result,'what happens')
 
 
 if __name__ == "__main__":
